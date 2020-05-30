4249367c0ae433edbf9937bf3a8ea923dcd07fd5
edieatha <edieatha@gmail.com>
2020-05-30 22:02:01 +0300

    Created class Scraper with methods

---
+++ lib/scraper.rb
@@ -0,0 +1,17 @@
+class Scraper
+  attr_accessor :parse_page
+
+  def initialize
+    url = 'https://www.apple.com/shop/refurbished/mac'
+    html = URI.open(url).read
+    @parse_page = Nokogiri::HTML(html)
+  end
+
+  def mac_specs
+    parse_page.css('h3').css('a').children.map { |detail| detail.text }.compact
+  end
+
+  def mac_prices
+    parse_page.css('.as-price-currentprice').children.map { |price| price.text }.compact
+  end
+end
