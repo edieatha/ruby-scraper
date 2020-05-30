456b3e79a6d076b53cf534481163e602cff748f7
edieatha <edieatha@gmail.com>
2020-05-30 22:06:41 +0300

    Updated main file to output scrape results

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,14 @@
 
 require 'open-uri'
 require 'nokogiri'
+
+require_relative '../lib/scraper'
+
+scraper = Scraper.new
+details = scraper.mac_specs
+prices = scraper.mac_prices
+
+(0...prices.size).each do |index|
+  puts "- - - index: #{index + 1} - - - "
+  puts "Details: #{details[index]}  | \n  Price:#{prices[index]} "
+end
