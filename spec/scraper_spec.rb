cb10406ea4813f464233fc21569a202eb44e2f86
edieatha <edieatha@gmail.com>
2020-06-02 15:03:22 +0300

    Fixed linter errors

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -16,23 +16,22 @@ describe Scraper do
       scraper = Scraper.new
       expect(scraper.mac_specs).to include 'Refurbished 13.3-inch MacBook Pro 2.3GHz quad-core Intel Core i5 with Retina display - Silver'
     end
-
   end
 
   describe '#prices' do
-      it 'Return prices as array.' do
-        scraper = Scraper.new
-        expect(scraper.mac_prices).to be_an_instance_of(Array)
-      end
-
-      it 'Mac prices of $1,300 are available!' do
-        scraper = Scraper.new
-        expect(scraper.mac_prices).to include /1,3./
-      end
-
-      it 'Return discounts as array.' do
-        scraper = Scraper.new
-        expect(scraper.amt_saved).to be_an_instance_of(Array)
-      end
+    it 'Return prices as array.' do
+      scraper = Scraper.new
+      expect(scraper.mac_prices).to be_an_instance_of(Array)
     end
+
+    it 'Mac prices of $1,300 are available!' do
+      scraper = Scraper.new
+      expect(scraper.mac_prices).to include(/1,3./)
+    end
+
+    it 'Return discounts as array.' do
+      scraper = Scraper.new
+      expect(scraper.amt_saved).to be_an_instance_of(Array)
+    end
+  end
 end
