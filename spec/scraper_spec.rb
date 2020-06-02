7587a1f882901e6d78c593c220482a97cf504505
edieatha <edieatha@gmail.com>
2020-06-02 14:00:21 +0300

    Changed redundant each_with_index to each

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -5,6 +5,7 @@ require_relative '../lib/scraper.rb'
 describe Scraper do
   url = 'https://www.apple.com/shop/refurbished/mac'
   html = URI.open(url).read
+
   describe '#mac_specs' do
     it 'Return specifications as array.' do
       scraper = Scraper.new
@@ -13,12 +14,12 @@ describe Scraper do
 
     it 'Mac Pro Computers are available!' do
       scraper = Scraper.new
-      expect(scraper.mac_specs).to include "Refurbished 13.3-inch MacBook Pro 2.3GHz quad-core Intel Core i5 with Retina display - Silver"
+      expect(scraper.mac_specs).to include 'Refurbished 13.3-inch MacBook Pro 2.3GHz quad-core Intel Core i5 with Retina display - Silver'
     end
 
   end
 
-    describe '#prices' do
+  describe '#prices' do
       it 'Return prices as array.' do
         scraper = Scraper.new
         expect(scraper.mac_prices).to be_an_instance_of(Array)
